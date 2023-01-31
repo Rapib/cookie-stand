da18c921c0f3511f15d00ab79fe01d46ce13623a
Thomas Lau <tomslau@outlook.com>
2023-01-31 15:29:05 -0800

    constructor and table

--- js/app.js
+++ js/app.js
@@ -2,6 +2,139 @@
 
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
+// lab 7
+
+// render top row HOURS
+function renderTopRow() {
+  let topTable = document.querySelector('table');
+  let topHead = document.createElement('thead');
+  let topTr = document.createElement('tr');
+  let topThE = document.createElement('th');
+  topThE.textContent = '';
+  topTable.appendChild(topHead);
+  topHead.appendChild(topTr);
+  topTr.appendChild(topThE);
+  for (let i = 0; i < hours.length; i++) {
+    let topTh = document.createElement('th');
+    topTh.textContent = hours[i];
+    topTable.appendChild(topHead);
+    topHead.appendChild(topTr);
+    topTr.appendChild(topTh);
+  }
+  let topTh = document.createElement('th');
+  topTh.textContent = 'Daily Location Total';
+  topTable.appendChild(topHead);
+  topHead.appendChild(topTr);
+  topTr.appendChild(topTh);
+}
+
+renderTopRow();
+
+// create constructor
+function Store(location, minCust, maxCust, avgSale) {
+  this.location = location;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgSale = avgSale;
+  this.rngCust = function () {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  };
+  this.rngSale = function () {
+    return Math.round((this.avgSale * this.rngCust()));
+  };
+  this.salesNoByHrAr = [];
+  this.calSalesNo = function () {
+    for (let i = 0; i < hours.length; i++) {
+      this.salesNoByHrAr.push(
+        this.rngSale());
+    }
+  };
+  this.totalSale = function () {
+    let total = 0;
+    for (let k = 0; k < this.salesNoByHrAr.length; k++) {
+      // console.log(this.salesNoByHrAr[k]);
+      total += this.salesNoByHrAr[k];
+    }
+    console.log(total);
+    return total;
+  };
+}
+
+
+
+// below are for render:
+Store.prototype.render = function () {
+  this.calSalesNo(); //cal sales number and store result in this.salesNoByHrAr
+  
+  let selectTable = document.querySelector('table');
+  let newHead = document.createElement('thead');
+  let newTr = document.createElement('tr');
+  let newTdName = document.createElement('td');
+  newTdName.textContent = this.location;
+  newTr.appendChild(newTdName);
+  selectTable.appendChild(newHead);
+  newHead.appendChild(newTr);
+  for (let i = 0; i < this.salesNoByHrAr.length; i++) {
+    let newTd = document.createElement('td');
+    newTd.textContent = this.salesNoByHrAr[i];
+    newTr.appendChild(newTd);
+    selectTable.appendChild(newHead);
+    newHead.appendChild(newTr);
+  }
+  let newTd = document.createElement('td');
+  newTd.textContent = this.totalSale();
+  newTr.appendChild(newTd);
+  selectTable.appendChild(newHead);
+  newHead.appendChild(newTr);
+};
+
+//create new store and test it
+let seattle = new Store('Seattle', 23, 65, 6.3);
+console.log(seattle);
+// seattle.calSalesNo();
+console.log(seattle.salesNoByHrAr);
+//render seattle
+seattle.render();
+
+// other stores:
+let tokyo = new Store('Tokyo', 3, 24, 1.2);
+tokyo.render();
+
+let dubai = new Store('Dubai', 11, 38, 3.7);
+dubai.render();
+
+let paris = new Store('Paris', 20, 38, 2.3);
+paris.render();
+
+let lima = new Store('Lima', 2, 16, 4.6);
+lima.render();
+
+
+/*
+
+// let newTable = document.querySelector('table'); //located the table from CSS, so Store name can be listed under
+// let thisP = document.createElement('p'); //created h2 for store name
+// thisP.textContent = this.location; //pull the store name from obj
+// newDiv.appendChild(seattleP); //add store nme under div
+
+// print list
+// for (let j = 0; j < hours.length; j++) {
+//   let seattleLi = document.createElement('li');
+//   seattleLi.textContent = `${hours[j]}: ${this.salesNoByHrAr[j]} cookies`;
+//   seattleP.appendChild(seattleLi);
+// }
+
+// let seattleLiTotal = document.createElement('li');
+// seattleLiTotal.textContent = `Total: ${this.totalSale()} cookies`;
+// seattleP.appendChild(seattleLiTotal);
+
+// console.log(seattle.salesNoByHrAr);
+
+
+
+
+
+// below are lab6 objs
 let seattleStore = {
   location: 'Seattle',
   minCust: 23,
@@ -32,10 +165,10 @@ let seattleStore = {
 
 };
 console.log(seattleStore.calSalesNo());
-console.log(seattleStore.salesNoByHrAr);
-console.log(seattleStore.rngCust());
-console.log(` ${seattleStore.rngSale()} is rngSale`);
-console.log(`total sales ${seattleStore.totalSale()}`);
+// console.log(seattleStore.salesNoByHrAr);
+// console.log(seattleStore.rngCust());
+// console.log(` ${seattleStore.rngSale()} is rngSale`);
+// console.log(`total sales ${seattleStore.totalSale()}`);
 
 let newDiv = document.getElementById('div'); //located the div on html, so Store name can be listed under
 // let newBody = document.body;
@@ -262,3 +395,4 @@ for (let j = 0; j < hours.length; j++) {
 let limaLiTotal = document.createElement('li');
 limaLiTotal.textContent = `Total: ${limaStore.totalSale()} cookies`;
 limaP.appendChild(limaLiTotal);
+*/
